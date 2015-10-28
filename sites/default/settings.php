668075095c429459a50a15f0571b784718ff75c6
Aurelie Rémia <Aurelie@MacBook-Pro-de-Aurelie.local>
2015-10-28 16:48:42 +0100

    Custom theme wip + debug:true

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -283,7 +283,7 @@ $config_directories = array();
  *   $settings['hash_salt'] = file_get_contents('/home/example/salt.txt');
  * @endcode
  */
-$settings['hash_salt'] = 'qIOfL6pzXaFiHB1IrdKaQMkkQzdrfy065TGcrpL81xuFlmAaJx4kRM2qyGXHrmasgn_BixEK-w';
+$settings['hash_salt'] = 'fkuuAIevrCS1QNWnmg9u8BXMP-JUAHONmYzeP5WWcZTnldDxrrxnj_gM7uyWK6uWkYHbHV28gQ';
 
 /**
  * Deployment identifier.
@@ -706,9 +706,8 @@ $settings['container_yamls'][] = __DIR__ . '/services.yml';
 # if (file_exists(__DIR__ . '/settings.local.php')) {
 #   include __DIR__ . '/settings.local.php';
 # }
-
 $databases['default']['default'] = array (
-  'database' => 'drupal8',
+  'database' => 'drupal',
   'username' => 'root',
   'password' => 'root',
   'prefix' => '',
@@ -718,4 +717,4 @@ $databases['default']['default'] = array (
   'driver' => 'mysql',
 );
 $settings['install_profile'] = 'standard';
-$config_directories['sync'] = 'sites/default/files/config_1NwJ9MP4PXxcSREVPI7cWXhBpoSXplZjC4GVwHNF-StKcWuAqu6NKtyYtk8PL640KWMkFyL5zg/sync';
+$config_directories['sync'] = 'sites/default/files/config_9wg_WfL9-84dhVgEXdIFrS0eiBtueSVMFsGLKD0kF2SykdciIHItsEMwwFlD-4wmSapI2aHzIA/sync';
